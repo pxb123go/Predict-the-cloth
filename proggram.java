648457542a6ecafe11d38f817eee5e6ef9c20770
pxb123go <sahoopradyun@gmail.com>
2024-10-04 19:49:45 +0300

    initial commit

---
+++ proggram.java
@@ -0,0 +1,15 @@
+class proggram {
+    public static void main(String[] args) {
+        boolean sunny=true;
+        boolean hot=false;
+        if (sunny) {
+          if (hot) {
+            System.out.println("Wear a cap and halve-sleeve t-shirt.");
+          } 
+          System.out.println("Wear a cap."); 
+        }
+        else {
+            System.out.println("Wear a Raincoat incase or a sweater.");
+        }
+    }
+}
